437ae627be7416b4ca4b925b8a7dbc4985a008a4
joeygaitan <joeygaitan707@gmail.com>
2020-04-13 18:29:15 -0700

    added in working whats up message

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,21 +7,22 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    def __init__(self):
-        self.question_text = models.CharField(max_length=200)
-        self.pub_date = models.DateTimeField('date published')
+   
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
 
-    # def was_published_recently(self):
-    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
 
 
 class Choice(models.Model):
-    def __init__(self):
-        self.question = models.ForeignKey(Question, on_delete=models.CASCADE)
-        self.choice_text = models.CharField(max_length=200)
-        self.votes = models.IntegerField(default=0)
+    
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
